7439337375c7a599c7629ace38e18b78e26d7314
yao80350 <yao80350@gmail.com>
2020-05-20 12:57:49 +0800

    send messages and show messages

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Grid } from "semantic-ui-react";
 
 import ColorPanel from "../ColorPanel/ColorPanel";
 import SidePanel from "../SidePanel/SidePanel";
 import Messages from "../Messages/Messages";
 import MetaPanel from "../MetaPanel/MetaPanel";
+import { StoreState } from "../../redux/reducers";
+import { Channel } from "../Channels/Channels";
 
-class Home extends React.Component {
+type HomeProps = {
+    currentChannel: null | Channel;
+    currentUser: null | firebase.User;
+};
+
+class Home extends React.Component<HomeProps> {
     render() {
+        const { currentChannel } = this.props;
         return (
             <Grid columns="equal" className="app home">
                 <ColorPanel />
                 <SidePanel />
                 <Grid.Column className="home__messages">
-                    <Messages />
+                    <Messages key={currentChannel?.id} {...this.props} />
                 </Grid.Column>
                 <Grid.Column width={4}>
                     <MetaPanel />
@@ -23,4 +32,12 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+const mapStateToProps = ({
+    channel: { currentChannel },
+    user: { currentUser },
+}: StoreState) => ({
+    currentChannel,
+    currentUser,
+});
+
+export default connect(mapStateToProps)(Home);
--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Segment, Comment } from "semantic-ui-react";
 
 import firebase from "../../firebase";
 import MessagesHeader from "../MessagesHeader/MessagesHeader";
 import MessageForm from "../MessageForm/MessageForm";
-import { StoreState } from "../../redux/reducers";
 import { Channel } from "../Channels/Channels";
 import Message from "../Message/Message";
 
@@ -31,15 +29,28 @@ class Messages extends React.Component<MessagesProps> {
         loading: true,
     };
 
+    componentDidMount() {
+        const { currentChannel, currentUser } = this.props;
+        if (currentChannel && currentUser) {
+            this.addListeners();
+        }
+    }
+
+    componentWillUnmount() {
+        const { currentChannel } = this.props;
+        currentChannel && this.state.messagesRef.child(currentChannel.id).off();
+    }
+
     addMessageListener = () => {
         const loadedMessages: MessageType[] = [];
         const { messagesRef } = this.state;
         const { currentChannel } = this.props;
-        if (!currentChannel) return;
-        messagesRef.child(currentChannel.id).on("child_added", (snap) => {
-            loadedMessages.push(snap.val());
-            this.setState({ messages: loadedMessages, loading: false });
-        });
+        currentChannel &&
+            messagesRef.child(currentChannel.id).on("child_added", (snap) => {
+                loadedMessages.push(snap.val());
+                console.log(this.state.messages);
+                this.setState({ messages: loadedMessages, loading: false });
+            });
     };
 
     addListeners = () => {
@@ -74,12 +85,4 @@ class Messages extends React.Component<MessagesProps> {
     }
 }
 
-const mapStateToProps = ({
-    channel: { currentChannel },
-    user: { currentUser },
-}: StoreState) => ({
-    currentChannel,
-    currentUser,
-});
-
-export default connect(mapStateToProps)(Messages);
+export default Messages;
